587be17f5dbb1408a5247e27c93765dff09f0cc3
Gavrevdokimov <64251638+Gavrevdokimov@users.noreply.github.com>
2020-10-21 12:52:33 +0300

    Update team_game.py

--- team_game.py
+++ team_game.py
@@ -8,8 +8,9 @@ import math
 import json
 import pandas as pd
 from threading import Timer
+import os
 
-token = "1043137499:AAGQgSKr1nk1sA5CMbB6fikDOrVrIi1zEdU"
+token = os.environ.get("BOT_TOKEN")
 bot = telebot.TeleBot(token)
 
 companies = {1: {'Standard Oil': 0, 'White Star Line': 0, 'Charron': 0, 'Путиловские заводы': 0, 'Grönvik glasbruk': 0},
